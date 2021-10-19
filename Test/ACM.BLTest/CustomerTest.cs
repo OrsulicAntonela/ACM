99b668ff6bcae5789a555c9ba9e2bb80de82ce34
Antonela Oršulić <aorsulic@hattrick.local>
2021-10-19 10:45:03 +0200

    Initial commit

---
+++ Test/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Ana",
+                LastName = "Anic"
+            };
+            string expected = "Anic, Ana";
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Anic"
+            };
+            string expected = "Anic";
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Ana"
+            };
+            string expected = "Ana";
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //-- Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Ana";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Ante";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Mate";
+            Customer.InstanceCount += 1;
+
+            //-- Act
+            //-- Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateValid() 
+        {
+            var customer = new Customer
+            {
+                LastName = "Antic",
+                EmailAddress = "aantic@gmail.com"
+            };
+
+            var expected = true;
+
+            var actual = customer.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
